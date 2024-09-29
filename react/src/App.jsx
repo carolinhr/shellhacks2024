4db6a1f66f790ede371ccbbae3a6031e922abc94
Carolin <carolinhromero@gmail.com>
2024-09-28 23:40:06 -0400

    fixing file status

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import HomePage from './Components/HomePage';
-import NavBar from './Components/NavBar';
-import QuizResult from './Components/QuizResult';
-
 
 const App = () => {
   return (
     <div>
-       <NavBar />
       <HomePage />
-      <QuizResult />
-      
-      
     </div>
-   
   );
 };
 
