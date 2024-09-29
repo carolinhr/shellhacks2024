f47d2f9b82b7d576e7a3a080c14220eed2fb9952
MariaPerez478 <mpere1074@fiu.edu>
2024-09-28 23:17:07 -0400

    updates

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import HomePage from './Components/HomePage';
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-</style>
+import NavBar from './Components/NavBar';
+import QuizResult from './Components/QuizResult';
+
 
 const App = () => {
   return (
     <div>
+       <NavBar />
       <HomePage />
+      <QuizResult />
+      
+      
     </div>
+   
   );
 };
 
