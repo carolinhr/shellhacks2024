99054b0dc64c78aee30ff108322e1a71fee7e744
MariaPerez478 <mpere1074@fiu.edu>
2024-09-28 21:45:02 -0400

    navbar

---
+++ react/src/Components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <header className="navBar">
+            <h1>HerTechPath</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/pathways">Pathways</a></li>
+                    <li><a href="/conect">Connect</a></li>
+
+
+                </ul>
+
+
+            </nav>
+            </header>
+
+
+
+            );
+        };
