bf89cf852bb66b86e89f9b905ded06332a90413b
Carolin <carolinhromero@gmail.com>
2024-09-28 23:51:34 -0400

    app file

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import HomePage from './Components/HomePage';
-
+import QuizResult from './Components/QuizResult';
 const App = () => {
   return (
     <div>
       <HomePage />
+      <QuizResult />
     </div>
   );
 };
