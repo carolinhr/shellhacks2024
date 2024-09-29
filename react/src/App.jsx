f27a31a2654c8b36d39e5963ec505e6975a4f3f7
Carolin <carolinhromero@gmail.com>
2024-09-28 23:06:54 -0400

    fonts

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import HomePage from './Components/HomePage';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+</style>
 
 const App = () => {
   return (
